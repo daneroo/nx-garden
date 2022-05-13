1e0bf0eabc3cc7154e63a15167c29c6aaef042f2
Daniel Lauzon <daniel.lauzon@gmail.com>
2022-05-13 11:45:19 -0400

    Update README (and site-e2e)

--- apps/site-e2e/src/integration/app.spec.ts
+++ apps/site-e2e/src/integration/app.spec.ts
@@ -1,7 +1,7 @@
 import { getGreeting } from '../support/app.po';
 
 describe('site', () => {
-  beforeEach(() => cy.visit('/'));
+  beforeEach(() => cy.visit('/oldindex'));
 
   it('should display welcome message', () => {
     // Custom command example, see `../support/commands.ts` file
